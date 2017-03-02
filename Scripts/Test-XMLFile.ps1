c060bdf02133f38615f23b247994b6e65d661dbf
Rajesh Yarlagadda <rayarlag@microsoft.com>
2017-03-02 10:56:19 -0800

    Validates XML file

---
+++ Scripts/Test-XMLFile.ps1
@@ -0,0 +1,25 @@
+﻿Function Test-XMLFile {
+    <#
+    .SYNOPSIS
+    Test the validity of an XML file
+    #>
+    [CmdletBinding()]
+    param (
+    [parameter(mandatory=$true)][ValidateNotNullorEmpty()][string]$xmlFilePath
+    )
+
+    # Check the file exists
+    if (!(Test-Path -Path $xmlFilePath)){
+    throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+    }
+    # Check for Load or Parse errors when loading the XML file
+    $xml = New-Object System.Xml.XmlDocument
+    try {
+    $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
+    return $true
+    }
+    catch [System.Xml.XmlException] {
+    Write-Host "$xmlFilePath : $($_.toString())"
+    return $false
+    }
+    }
