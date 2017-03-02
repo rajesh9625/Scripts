ef4eb503c78a89fcdf58dacd1d395976b9c1cf31
Rajesh Yarlagadda <rayarlag@microsoft.com>
2017-03-02 13:30:48 -0800

    refactored both the scripts

--- Scripts/Test-JSONFile.ps1
+++ Scripts/Test-JSONFile.ps1
@@ -3,23 +3,26 @@
     .SYNOPSIS
     Test the validity of an JSON file
     #>
+    
     [CmdletBinding()]
     param (
-    [parameter(mandatory=$true)][ValidateNotNullorEmpty()][string]$JsonFilePath
+    [parameter(mandatory=$true)][ValidateNotNull()][string]$JsonFilePath
     )
 
     # Check the file exists
-    if (!(Test-Path -Path $JsonFilePath)){
+    if (!(Test-Path -Path $JsonFilePath)) {
     throw "$JsonFilePath is not valid. Please provide a valid path to the .json file"
     }
+    
     # Checking for Load or Parse errors when loading the JSON file
     try {
     $text = Get-Content $JsonFilePath -Raw 
-    $powershellRepresentation = ConvertFrom-Json $text -ErrorAction Stop;
+    $JSONRepresentation = ConvertFrom-Json $text -ErrorAction Stop;
     return $true
     }
+    
     catch {
     Write-Error $error[0]
     return $false 
     }
-    }
+}
--- Scripts/Test-XMLFile.ps1
+++ Scripts/Test-XMLFile.ps1
@@ -3,24 +3,26 @@
     .SYNOPSIS
     Test the validity of an XML file
     #>
+    
     [CmdletBinding()]
     param (
-    [parameter(mandatory=$true)][ValidateNotNullorEmpty()][string]$XmlFilePath
+    [parameter(mandatory=$true)][ValidateNotNull()][string]$XmlFilePath
     )
 
     # Check the file exists
-    if (!(Test-Path -Path $XmlFilePath)){
+    if (!(Test-Path -Path $XmlFilePath)) {
     throw "$XmlFilePath is not valid. Please provide a valid path to the .xml file"
     }
+    
     # Checking for Load or Parse errors when loading the XML file
     $xml = New-Object System.Xml.XmlDocument
     try {
     $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
     return $true
     }
+    
     catch [System.Xml.XmlException] {
     Write-Error $_.Exception.Message
-    #echo $_.Exception|format-list -force
     return $false
     }
-    }
+}
